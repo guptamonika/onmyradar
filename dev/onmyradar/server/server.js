ea9a19c8e7895d86baf39c608c59d8da262d467e
Monika Gupta <monikag@yahoo-inc.com>
2017-01-27 14:30:35 -0800

    Initial Commit with the Main Header Item

---
+++ dev/onmyradar/server/server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var path = require("path");
+var morgan = require('morgan') // logger
+
+var app = express();
+app.use(express.static(path.join(__dirname,"../app/dist"))); // static files like css etc
+
+/*app.get('/', function (req, res) {
+  res.send('Hello World!')
+})*/
+
+app.use(morgan('combined')); // log
+app.listen(7777,function(){
+    console.log("Started listening on port", 7777);
+})
